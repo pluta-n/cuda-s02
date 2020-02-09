aeab411cfd42ad3b452d3f7cac13bfcefbe6b03f
pluta-n <56379957+pluta-n@users.noreply.github.com>
2020-02-09 21:39:40 +0100

    Add files via upload

---
+++ PROJECT/pprjct/Debug/src/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CU_SRCS += \
+../src/pprjct.cu 
+
+OBJS += \
+./src/pprjct.o 
+
+CU_DEPS += \
+./src/pprjct.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	/usr/local/cuda-10.0/bin/nvcc -G -g -O0 -gencode arch=compute_61,code=sm_61  -odir "src" -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-10.0/bin/nvcc -G -g -O0 --compile --relocatable-device-code=false -gencode arch=compute_61,code=compute_61 -gencode arch=compute_61,code=sm_61  -x cu -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
